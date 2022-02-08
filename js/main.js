fadeb66693b35c5d03c8f198f28dbe11bed1035b
eqodel <eqodel@mail.ru>
2022-02-08 18:13:07 +0300

    create: modal

--- js/main.js
+++ js/main.js
@@ -39,6 +39,25 @@ $('.welcome__video').parent().click(function () {
 
   var menuButton = $('.menu__button');
   menuButton.on('click', function () {
-    console.log('click');
     $('.nav').toggleClass('nav--visible');
   });
+
+  var modalButton = $('.button');
+  var closeModalButton = $('.modal__close');
+  modalButton.on("click", openModal);
+  closeModalButton.on('click', closeModal);
+    
+  function openModal() {
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.addClass('modal__overlay--visible');
+    modalDialog.addClass('modal__dialog--visible');
+  };
+
+  function closeModal(event) {
+    event.preventDefault()
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible');
+  };
